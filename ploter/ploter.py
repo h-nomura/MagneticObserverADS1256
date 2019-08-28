eab6d3935ba4bcc8470088833a5ee10636247fa2
Nomura Hiroshi <h_nomura@echo.nuee.nagoya-u.ac.jp>
2019-08-28 13:09:50 +0900

    Update ploter.py complete You can overlay average and raw data graphs.

--- ploter/ploter.py
+++ ploter/ploter.py
@@ -7,9 +7,16 @@ import pandas as pd
 import sys
 import datetime
 
+import matplotlib as mpl
+mpl.rcParams['agg.path.chunksize'] = 100000
+
 def eliminate_f(date_str):
     date = datetime.datetime.strptime(date_str, '%H:%M:%S.%f')
     return date.strftime('%H:%M:%S')
+
+def format_to_day(date_str):
+    date = datetime.datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
+    return date.strftime('%Y-%m-%d ')
 
 def search_start(dataTime,start_dataTime_str):
     i = 0
@@ -33,23 +40,26 @@ def search_end(dataTime,end_dataTime_str,start_arg):
 
 def df_maker(f,start_dataTime_str,end_dataTime_str,rawFlag):
     df_list = {'dataTime':[],'data':[]}
+    df_list_raw = {'dataTime':[],'data':[]}
     SAMdata = 0
     NUMdata = 0
     NOWdate = ''
     SPrate = 0
     startFlag = False
     endFlagNum = False
+    dataday = format_to_day(start_dataTime_str)
+    average = 0
     for row in f:#row is list
         # print('2019-08-09 ' + eliminate_f(row[0]))
         if startFlag == False:
-            if ('2019-08-09 ' + eliminate_f(row[0])) == start_dataTime_str:
+            if (dataday + eliminate_f(row[0])) == start_dataTime_str:
                 print("START")
                 startFlag = True
         if startFlag == True:
-            if ('2019-08-09 ' + eliminate_f(row[0])) == end_dataTime_str:
+            if (dataday + eliminate_f(row[0])) == end_dataTime_str:
                 endFlagNum = True
         if endFlagNum == True:
-            if ('2019-08-09 ' + eliminate_f(row[0])) != end_dataTime_str:
+            if (dataday + eliminate_f(row[0])) != end_dataTime_str:
                 print("END")
                 endFlagNum = False
                 startFlag = False
@@ -57,31 +67,50 @@ def df_maker(f,start_dataTime_str,end_dataTime_str,rawFlag):
         
         if rawFlag == 'RAW':
             if startFlag == True:
-                df_list['dataTime'].append('2019-08-09 ' + row[0])
-                df_list['data'].append(float(row[1]))
+                df_list_raw['dataTime'].append(dataday + row[0])
+                df_list_raw['data'].append(float(row[1]))
+                average += float(row[1])
+                average /= 2
         elif rawFlag == '':
             SAMdata += float(row[1])
             NUMdata += 1
             if NOWdate != '':
                 if startFlag == True:
                     if NOWdate != eliminate_f(row[0]):
-                        df_list['dataTime'].append('2019-08-09 ' + NOWdate)
+                        df_list['dataTime'].append(dataday + NOWdate)
+                        df_list['data'].append(SAMdata / NUMdata)
+                        average += (SAMdata / NUMdata)
+                        average /= 2
+                        SAMdata = float(row[1])
+                        SPrate = NUMdata
+                        NUMdata = 1
+            NOWdate = eliminate_f(row[0])
+        elif rawFlag == 'OVER':
+            if startFlag == True:
+                df_list_raw['dataTime'].append(dataday + row[0])
+                df_list_raw['data'].append(float(row[1]))
+                average += float(row[1])
+                average /= 2
+            SAMdata += float(row[1])
+            NUMdata += 1
+            if NOWdate != '':
+                if startFlag == True:
+                    if NOWdate != eliminate_f(row[0]):
+                        df_list['dataTime'].append(dataday + NOWdate)
                         df_list['data'].append(SAMdata / NUMdata)
                         SAMdata = float(row[1])
                         SPrate = NUMdata
                         NUMdata = 1
             NOWdate = eliminate_f(row[0])
-    print(SPrate)
-    df = pd.DataFrame({
-        'date time':pd.to_datetime(df_list['dataTime']),
-        'Magnetic force':df_list['data']
-    })
-    return df
+
+    if rawFlag == '':
+        print(SPrate)
+
+    return df_list['dataTime'],df_list['data'],df_list_raw['dataTime'],df_list_raw['data'],average
 
 #ex. start_datetime_str = 2017-08-01 01:00:00
-def fig_plot(f,start_datetime_str,end_datetime_str,fig_size,rawFlag,ymin,ymax):
-    df = df_maker(f,start_datetime_str,end_datetime_str,rawFlag)
-    df = df.set_index('date time')
+def fig_plot(f,start_datetime_str,end_datetime_str,fig_size,rawFlag,ymin,ymax,Yrange):
+    dfList = df_maker(f,start_datetime_str,end_datetime_str,rawFlag)
     # Figureの初期化
     if fig_size == 's':
         fig = plt.figure(figsize=(12, 8))
@@ -91,10 +120,21 @@ def fig_plot(f,start_datetime_str,end_datetime_str,fig_size,rawFlag,ymin,ymax):
         fig = plt.figure(figsize=(12, 4.5))
     # Figure内にAxesを追加()
     ax = fig.add_subplot(111) #...2
-    if ymin != 0:
-        ax.set_ylim([ymin,ymax])
+    if Yrange != 0:
+        ax.set_ylim([dfList[4] - (Yrange/2),dfList[4] + (Yrange/2)])
     ax.yaxis.grid(True)
-    ax.plot(df.index,df['Magnetic force'])
+    if rawFlag == 'RAW' or rawFlag == 'OVER':
+        df = pd.DataFrame({
+            'date time raw':pd.to_datetime(dfList[2]),
+            'Magnetic force raw':dfList[3]
+        })
+        ax.plot(df['date time raw'], df['Magnetic force raw'], color='b')
+    if rawFlag == '' or rawFlag == 'OVER':
+        df = pd.DataFrame({
+            'date time':pd.to_datetime(dfList[0]),
+            'Magnetic force':dfList[1]
+        })
+        ax.plot(df['date time'], df['Magnetic force'], color='r')
     ax.set_title(start_datetime_str + '(JST) to ' + end_datetime_str + '(JST) ' + 'northward component of magnetic force(nT)' + rawFlag)
     fig_dir = datetime.datetime.strptime(start_datetime_str, '%Y-%m-%d %H:%M:%S')
     end_dir = datetime.datetime.strptime(end_datetime_str, '%Y-%m-%d %H:%M:%S')
@@ -116,21 +156,31 @@ def rewrite_day(reference_date,num):
 
 def hour_fig_plot(f,reference_date,num,fig_size):
     for i in range(num):
-        fig_plot(f,rewrite_day(reference_date,i),rewrite_day(reference_date,i+1),fig_size,'',0,0)
+        fig_plot(f,rewrite_day(reference_date,i),rewrite_day(reference_date,i+1),fig_size,'',0,0,0)
 
-def main():
-    print("aa")
-    args = sys.argv
-    if len(args) != 2:
-        print("write ==python ploter.py csvFilePass")
-        sys.exit(1)
-    csv_file = open(args[1],"r",encoding = "ms932",errors = "", newline = "")
+def Process(fileName,StartTime,EndTime,rawFlag,ymin,ymax,Yrange):
+    Pass = "..\\logger\\data\\" + fileName
+    csv_file = open(Pass,"r",encoding = "ms932",errors = "", newline = "")
     f = csv.reader(csv_file, delimiter=",",doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
     header = next(f)
     print(header)
-    # hour_fig_plot(f,'2019-06-20 23:30:00',15,'l')
-    # fig_plot(f,'2019-08-04 00:00:00','2019-08-04 12:00:00','l','',0,0)
-    fig_plot(f,'2019-08-09 20:21:00','2019-08-09 20:23:00','l','RAW',18500,19500)
+    fig_plot(f,header[0] + ' ' + StartTime ,header[0] + ' ' + EndTime,'l', rawFlag, ymin, ymax ,Yrange)
+
+def main():
+    File = [
+    "MI19-08-27_19h53m21s.csv",
+    "MI19-08-20_15h41m40s.csv",
+    "MI19-08-20_15h56m19s.csv",
+    "MI19-08-20_16h10m42s.csv",
+    "MI19-08-20_16h23m17s.csv",
+    "MI19-08-20_16h39m01s.csv"]
+    Process(File[0],"19:54:00","19:55:00","OVER",0,0,5000)
+    # Process(File[1],"15:42:00","15:52:00","",0,0,5000)
+    # Process(File[2],"15:57:00","16:07:00","",0,0,5000)
+    # Process(File[3],"16:11:00","16:21:00","",0,0,5000)
+    # Process(File[4],"16:24:00","16:34:00","",0,0,5000)
+    # Process(File[5],"16:40:00","16:50:00","",0,0,5000)
+
 
 if __name__ == '__main__':
     main()
