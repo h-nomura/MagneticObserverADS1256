4f77154705f024d1db136cce2a70598517068439
Nomura Hiroshi <h_nomura@echo.nuee.nagoya-u.ac.jp>
2019-09-25 18:37:17 +0900

    Complete test.py and Update realtiomePolotLogger.py

--- logger/realtimePlotLogger.py
+++ logger/realtimePlotLogger.py
@@ -1,88 +1,79 @@
-import sys
-import os
-from ADS1256_definitions import *
-from pipyadc import ADS1256
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-import time, datetime
-import csv
+import time
+import threading
+import math
+import itertools
 
-if not os.path.exists("/dev/spidev0.1"):
-    raise IOError("Error: No SPI device. Check settings in /boot/config.txt")
-# Input pin for the potentiometer on the Waveshare Precision ADC board:
-Diff0_1 = POS_AIN0|NEG_AIN1
-Diff2_3 = POS_AIN2|NEG_AIN3
-Diff4_5 = POS_AIN4|NEG_AIN5
-Diff6_7 = POS_AIN6|NEG_AIN7
-CH_SEQUENCE = (Diff0_1,Diff2_3,Diff4_5,Diff6_7)
-def do_measurement():
-    ### STEP 1: Initialise ADC object using default configuration:
-    # (Note1: See ADS1256_default_config.py, see ADS1256 datasheet)
-    # (Note2: Input buffer on means limited voltage range 0V...3V for 5V supply)
-    ads = ADS1256()
-    # サンプリング・レートの設定
-    # REG_DRATE: Sample rate definitions:
-    # DRATE_30000     = 0b11110000 # 30,000SPS (default)
-    # DRATE_15000     = 0b11100000 # 15,000SPS
-    # DRATE_7500      = 0b11010000 # 7,500SPS
-    # DRATE_3750      = 0b11000000 # 3,750SPS
-    # DRATE_2000      = 0b10110000 # 2,000SPS
-    # DRATE_1000      = 0b10100001 # 1,000SPS
-    # DRATE_500       = 0b10010010 # 500SPS
-    # DRATE_100       = 0b10000010 # 100SPS
-    # DRATE_60        = 0b01110010 # 60SPS
-    # DRATE_50        = 0b01100011 # 50SPS
-    # DRATE_30        = 0b01010011 # 30SPS
-    # DRATE_25        = 0b01000011 # 25SPS
-    # DRATE_15        = 0b00110011 # 15SPS
-    # DRATE_10        = 0b00100011 # 10SPS
-    # DRATE_5         = 0b00010011 # 5SPS
-    # DRATE_2_5       = 0b00000011 # 2.5SPS
-    ads.drate = DRATE_1000
-    # gainの設定
-    ads.pga_gain = 1
-    ### STEP 2: Gain and offset self-calibration:
-    ads.cal_self()
+from matplotlib import pyplot as plt
+from matplotlib import animation
 
-    while True:
-        now = datetime.datetime.now()#get time
-        today = '{0:%Y-%m-%d}'.format(now)
-        with open('./data/MI{0:%y-%m-%d_%Hh%Mm%Ss}.csv'.format(now),'w') as f:
-            data = ['{0:%Y-%m-%d}'.format(now),
-            'Magnetic force(nT)_1ch','Magnetic force(nT)_2ch',
-            'Magnetic force(nT)_3ch','Magnetic force(nT)_4ch']
-            writer = csv.writer(f)
-            writer.writerow(data)
-            counter = 0
-            while True:            
-                now = datetime.datetime.now()
-                # get data
-                raw_channels = ads.read_sequence(CH_SEQUENCE)
-                voltages     = [(i * ads.v_per_digit * 6.970260223 - 15.522769516) for i in raw_channels]
-                MagneticF     = [(i * 1000 / 0.16) for i in voltages]
 
-                data = ['{0:%H:%M:%S.%f}'.format(now),
-                MagneticF[0], MagneticF[1],
-                MagneticF[2], MagneticF[3]]
-                if counter == 1000:
-                    print('{0:%Y-%m-%d  %H:%M:%S}'.format(now) + '  Magnetic force(nT)==' + str(MagneticF[0]))
-                    counter = 0
-                writer = csv.writer(f)
-                writer.writerow(data)
-                counter += 1
-                if '{0:%Y-%m-%d}'.format(now) != today:
-                    break
-                today = '{0:%Y-%m-%d}'.format(now)
+def _redraw(_, x, y, x2, y2):
+    """グラフを再描画するための関数"""
+    # 現在のグラフを消去する
+    plt.cla()
+    # 折れ線グラフを再描画する
+    fig1 = plt.plot(x, y)
+    fig2 = plt.plot(x2, y2)
+    return fig1 + fig2
+
+
+def show_graph():
+    # 描画領域
+    fig = plt.figure(figsize=(12, 6))
+    # 描画するデータ (最初は空っぽ)
+    x = []
+    y = []
+    x2 = []
+    y2 = []
+
+    def _update():
+        """データを一定間隔で追加するスレッドの処理"""
+        for frame in itertools.count(0, 0.1):
+            x.append(frame)
+            y.append(math.sin(frame))
+            x2.append(frame)
+            y2.append(math.cos(frame))
+            # データを追加する間隔 (100ms)
+            time.sleep(0.1)
+
+    def _init():
+        """データを一定間隔で追加するためのスレッドを起動する"""
+        t = threading.Thread(target=_update)
+        t.daemon = True
+        t.start()
+
+    params = {
+        'fig': fig,
+        'func': _redraw,  # グラフを更新する関数
+        'init_func': _init,  # グラフ初期化用の関数 (今回はデータ更新用スレッドの起動)
+        'fargs': (x, y, x2, y2),  # 関数の引数 (フレーム番号を除く)
+        'interval': 250,  # グラフを更新する間隔 (ミリ秒)
+    }
+    animation.FuncAnimation(**params)
+
+    # グラフを表示する
+    plt.show()
 
 def main():
-    try:
-        # print("\033[2J\033[H") # Clear screen
-        print(__doc__)
-        print("\nPress CTRL-C to exit.")
-        do_measurement()
+    while True:#set config
+        print("Do you want to save the observation data?(yes/no)")
+        flag_save = input('>> ')
 
-    except (KeyboardInterrupt):
-        print("\n"*8 + "User exit.\n")
-        sys.exit(0)
+        print("Set the graph parameters to be displayed. \nEnter the magnetic Force range.(nT,10-80000)")
+        config_range = int(input('>> '))
+        print("Select graph to display(1s average/raw/overlay)")
+        config_mode = input('>> ')
+        if flag_save == "yes" or flag_save == "no":
+            if 10 <= config_range <= 80000:
+                if config_mode == "1s average" or config_mode == "raw" or config_mode == "overlay":
+                    break
+        print("######input errer#######\ntry again!!")
+    show_graph()
+
+
 
 if __name__ == '__main__':
-	main()
+    main()
