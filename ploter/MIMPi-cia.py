a6d6999b4f64cea243252da062302ff4f525c6ef
Nomura Hiroshi <h_nomura@echo.nuee.nagoya-u.ac.jp>
2020-10-27 18:55:32 +0900

    Update process and proter

--- ploter/MIMPi-cia.py
+++ ploter/MIMPi-cia.py
@@ -219,7 +219,6 @@ def test_plot(x):
     fig.tight_layout()
     plt.show()
 
-
 def fig_plot(df_print,labelList, title, fig_path, F_flag, dat_path = '', Yrange = 0):
     fig = plt.figure(figsize=(12, 12))
     ax_1ch = fig.add_subplot(611)
@@ -399,9 +398,9 @@ def ICA_process2(y1,y2,y3,y4,y5,y6):
     y1 = np.dot(S,A_)[:,0] + y1mean
     y2 = np.dot(S,A_)[:,1] + y2mean
     y3 = np.dot(S,A_)[:,2] + y3mean
-    y4 = np.dot(N,A_)[:,3] + y4mean
-    y5 = np.dot(N,A_)[:,4] + y5mean
-    y6 = np.dot(N,A_)[:,5] + y6mean
+    y4 = np.dot(N,A_)[:,0] + y1mean
+    y5 = np.dot(N,A_)[:,1] + y2mean
+    y6 = np.dot(N,A_)[:,2] + y3mean
     test_plot([y1,y2,y3,y4,y5,y6])
     return [x[0],x[1],x[2],x[3],x[4],x[5]], [y1,y2,y3,y4,y5,y6,choiceNUM]
 
@@ -468,7 +467,7 @@ def check_errerData(date_numpy,start_datetime):
 def Process(fileName,StartTime,EndTime, F_flag ,Yrange):
     siteInfo = ""
     df_print = []
-    for i in range(2):
+    for i in range(3):
         Pass = "../logger/data/" + fileName[i]
         siteInfo += crop_str(crop_str(fileName[i],"@"),".",mode=1)
         csv_file = open(Pass,"r",encoding = "ms932",errors = "", newline = "")
@@ -504,7 +503,7 @@ def Process(fileName,StartTime,EndTime, F_flag ,Yrange):
         ana_print.append(pd.DataFrame({'time':df_print[0]['time'],'1ch':ica_result1[0],'2ch':ica_result1[1],'3ch':ica_result1[2],'4ch':df_print[0]['4ch']}))
         ana_print.append(pd.DataFrame({'time':df_print[1]['time'],'1ch':ica_result1[3],'2ch':ica_result1[4],'3ch':ica_result1[5],'4ch':df_print[1]['4ch']}))
         ana_print2.append(pd.DataFrame({'time':df_print[0]['time'],'1ch':ica_result2[0],'2ch':ica_result2[1],'3ch':ica_result2[2],'4ch':df_print[0]['4ch']}))
-        ana_print2.append(pd.DataFrame({'time':df_print[1]['time'],'1ch':ica_result2[3],'2ch':ica_result2[4],'3ch':ica_result2[5],'4ch':df_print[1]['4ch']}))
+        ana_print2.append(pd.DataFrame({'time':df_print[2]['time'],'1ch':df_print[2]['1ch'],'2ch':df_print[2]['2ch'],'3ch':df_print[2]['3ch'],'4ch':df_print[1]['4ch']}))
     elif F_flag == "PCA":
         ica_result_x = PCA_process_2sig(df_print[0]['1ch'],df_print[1]['1ch'])
         ica_result_y = PCA_process_2sig(df_print[0]['2ch'],df_print[1]['2ch'])
@@ -519,7 +518,7 @@ def Process(fileName,StartTime,EndTime, F_flag ,Yrange):
 
     fig_date = datetime.datetime.strptime(start_time_str, '%Y-%m-%d %H:%M:%S')
     end_dir = datetime.datetime.strptime(end_time_str, '%Y-%m-%d %H:%M:%S')
-    fig_dir = './fig/' + fig_date.strftime('%Y-%m-%d') + siteInfo
+    fig_dir = './fig/SGEPSS_ICA/' + fig_date.strftime('%Y-%m-%d') + siteInfo
     figFileDate = fig_date.strftime('%Y-%m-%d_%H%M%S') + end_dir.strftime('-%H%M%S')
     my_makedirs(fig_dir)
     title = start_time_str + '(UT) magnetic force(nT)' + F_flag + siteInfo
@@ -530,8 +529,9 @@ def Process(fileName,StartTime,EndTime, F_flag ,Yrange):
         fig_plot(ana_print,labelList,title, fig_path,F_flag, Yrange=Yrange)
 
     if F_flag == 'ICA':
-        labelList = ['X [nT]','Y [nT]','Z [nT]','Noise of X [nT]','Noise of Y [nT]','Noise of Z [nT]']
-        labelList2 = ['X [nT]','Y [nT]','Z [nT]','Noise of X [nT]','Noise of Y [nT]','Noise of Z [nT]']
+        labelList = ['component1','component2','component3','component4','component5','component6']
+        # labelList2 = ['X [nT]','Y [nT]','Z [nT]','Noise of X [nT]','Noise of Y [nT]','Noise of Z [nT]']
+        labelList2 = ['X of MIM-Pi No1 [nT]','Y of MIM-Pi No1 [nT]','Z of MIM-Pi No1 [nT]','X of Fluxgate [nT]','Y of Fluxgate [nT]','Z of Fluxgate [nT]']
         fig_path1 = fig_dir + '/' + figFileDate + '_' + str(Yrange)+F_flag+"_analysis_component"+siteInfo+'.png'
         fig_path2 = fig_dir + '/' + figFileDate + '_' + str(Yrange)+F_flag+"_analysis_Result"+ica_result2[6]+siteInfo+'.png'
         fig_plot(ana_print,labelList,title, fig_path1,F_flag, Yrange=0)
@@ -571,14 +571,14 @@ def main():
     File = [
     "1sec_median_MI20-10-24_00h00m00s@inabu_byNo1.csv",
     "1sec_median_MI20-10-24_00h00m00s@inabu_byNo2.csv",
-    "MI19-09-20_12h39m47s.csv"]
+    "Fx20-10-24_00h00m00s@inabu_Flux.csv"]
     # Process([File[0],File[1]],"00:00:00","23:59:59","median",200)
     # Process([File[0],File[1]],"00:00:00","03:00:00","median",100)
     # Process([File[0],File[1]],"03:00:00","06:00:00","median",100)
     # Process([File[0],File[1]],"06:00:00","09:00:00","median",100)
     # Process([File[0],File[1]],"09:00:00","12:00:00","median",100)
     # Process([File[0],File[1]],"12:00:00","15:00:00","median",100)
-    Process([File[0],File[1]],"19:10:00","19:20:00","ICA",10)
+    Process([File[0],File[1],File[2]],"19:00:00","20:00:00","ICA",10)
     # Process([File[0],File[1]],"18:00:00","21:00:00","PCA",10)
     # day_1hour([File[0],File[1]],"PCA",20)
 
